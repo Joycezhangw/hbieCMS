86adb9c7420173e0a8e68e79733aa8a75d146dfd
joycezhang <787027175@qq.com>
2020-07-10 17:21:08 +0800

    如果当前路由不是菜单，者获取上级路由

--- app/Http/Controllers/Manage/ManageController.php
+++ app/Http/Controllers/Manage/ManageController.php
@@ -21,7 +21,7 @@ class ManageController extends Controller
         $manageModuleRepo = app(IManageModule::class);
         $module = $templatePath['module'];
         $user = Auth::guard('admin')->user()->toArray();
-        $user['manage_avatar']=FiltersHelper::buildImageUri($user['manage_avatar']);
+        $user['manage_avatar'] = FiltersHelper::buildImageUri($user['manage_avatar']);
         //根据用户信息获取匹配的菜单数据，和全部权限数据。用户权限判断
         $moduleList = $manageModuleRepo->getModuleAuth($module, $user);
         $menu_list = [];
@@ -34,9 +34,18 @@ class ManageController extends Controller
         $route = $manageModuleRepo->getLastModuleByRoute($current_route);
         //根据当前路由，获取他全部上级路由数据
         $menuParent = TreeHelper::getParents($moduleList['authList'], $route['module_id'], 'module_id');
+
+        //判断当前路由是否是菜单路由，如果是菜单路由，则选中当前路由，如果不是，则获取上级路由。且路由不能超过4级
+        if (intval($route['is_menu']) === 1) {
+            $currentRoute = $current_route;
+        } else {
+            $parent = TreeHelper::getParent($moduleList['authList'], $route['module_id'], 'module_id');
+            $currentRoute = count($parent) > 0 ? $parent[0]['module_route'] : $current_route;
+        }
+
         return [
-            'admin_user'=>$user,
-            'current_route' => $current_route,//获取当前路由，用于定位菜单选中状态
+            'admin_user' => $user,
+            'current_route' => $currentRoute,//获取当前路由，用于定位菜单选中状态
             'current_route_pid' => isset($menuParent[0]) ? $menuParent[0]['module_id'] : 0,//获取最顶级父路由id，用户标识顶级菜单状态
             'title_name' => $route['module_name'] . '-' . '马尾留学生创业园',//网页title
             'menu_list' => $menu_list,//导航菜单
