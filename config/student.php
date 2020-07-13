7198f0cff1db958377a1b237fd8dad020b7ddb19
joycezhang <787027175@qq.com>
2020-07-13 18:43:02 +0800

    配置文件

---
+++ config/student.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'title' => '马尾留学生创业园',
+    'posts_per_page' => 5,
+    'paginate' => [
+        'page_size' => 15
+    ],
+    'uploads' => [
+        'storage' => 'uploads',
+        'webPath' => '/uploads',
+    ],
+    'course_index_weight' => 1
+];
