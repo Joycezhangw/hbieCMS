ca5a102983e9f17ae3d76cda74f145b89258cf39
joycezhang <787027175@qq.com>
2020-07-14 14:01:26 +0800

    不自动维护时间戳

--- app/Services/Models/CMS/PostTag.php
+++ app/Services/Models/CMS/PostTag.php
@@ -9,6 +9,12 @@ class PostTag extends Model
 {
     protected $table='hb_cms_post_tag';
 
+    /**
+     * 指示是否自动维护时间戳
+     * @var bool
+     */
+    public $timestamps = false;
+
     protected $fillable=[
         'tag_id','post_id'
     ];
