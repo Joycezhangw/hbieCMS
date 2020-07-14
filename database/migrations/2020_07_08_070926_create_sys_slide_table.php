378ae563bb93867fed3b32140d8b653a6cc534b8
joycezhang <787027175@qq.com>
2020-07-14 16:14:18 +0800

    增加字段

--- database/migrations/2020_07_08_070926_create_sys_slide_table.php
+++ database/migrations/2020_07_08_070926_create_sys_slide_table.php
@@ -19,6 +19,7 @@ class CreateSysSlideTable extends Migration
             $table->string('slide_pic')->default('幻灯片图片地址');
             $table->string('slide_page')->default('小程序跳转地址');
             $table->unsignedTinyInteger('is_show')->default(1)->comment('是否展示[1 是 0 否]');
+            $table->unsignedInteger('slide_sort')->default(0)->comment('排序，越小靠前 ');
             $table->string('slide_desc',512)->default('')->comment('幻灯片描述');
             $table->unsignedInteger('created_at')->default(0)->comment('创建时间');
             $table->unsignedInteger('updated_at')->default(0)->comment('更新时间');
