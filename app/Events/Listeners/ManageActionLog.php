65581296c64a6d0e93a01741f51e5dd4d5084706
joycezhang <787027175@qq.com>
2020-07-18 14:13:52 +0800

    事件handle 改成仓库调用模式

--- app/Events/Listeners/ManageActionLog.php
+++ app/Events/Listeners/ManageActionLog.php
@@ -3,9 +3,7 @@
 namespace App\Events\Listeners;
 
 use App\Events\ManageAction;
-use App\Services\Models\Manage\ManageLog;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Queue\InteractsWithQueue;
+use App\Services\Repositories\Manage\Interfaces\IManageLog;
 use JoyceZ\LaravelLib\Helpers\StrHelper;
 
 class ManageActionLog
@@ -28,15 +26,16 @@ class ManageActionLog
      */
     public function handle(ManageAction $event)
     {
-        $log = new ManageLog();
-        $log->log_ip = StrHelper::ip2long($event->log_ip);
-        $log->manage_id = $event->manage_id;
-        $log->manage_username = $event->username;
-        $log->log_url = $event->log_url;
-        $log->log_title = $event->log_title;
-        $log->log_content = json_encode($event->log_content);
-        $log->useragent = $event->useragent;
-        $log->save();
+        $IManageLogRepo = app(IManageLog::class);
+        $IManageLogRepo->doCreate([
+            'log_ip' => StrHelper::ip2long($event->log_ip),
+            'manage_id' => $event->manage_id,
+            'manage_username' => $event->username,
+            'log_url' => $event->log_url,
+            'log_title' => $event->log_title,
+            'log_content' => json_encode($event->log_content),
+            'useragent' => $event->useragent
+        ]);
 
     }
 }
