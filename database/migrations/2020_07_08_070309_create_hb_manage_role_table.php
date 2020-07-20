876ac76d4a66c0a9fbd4babfdb4e8db824166911
joycezhang <787027175@qq.com>
2020-07-20 11:11:38 +0800

    增加角色名称唯一索引

--- database/migrations/2020_07_08_070309_create_hb_manage_role_table.php
+++ database/migrations/2020_07_08_070309_create_hb_manage_role_table.php
@@ -15,11 +15,13 @@ class CreateHbManageRoleTable extends Migration
     {
         Schema::create('hb_manage_role', function (Blueprint $table) {
             $table->increments('role_id')->comment('管理员角色主键id');
-            $table->string('role_title',50)->default('')->comment('角色名称');
+            $table->string('role_title', 50)->default('')->comment('角色名称');
             $table->unsignedTinyInteger('is_default')->default(0)->comment('是否默认角色[1 是 0 否]');
-            $table->string('role_desc',512)->default('')->comment('角色描述');
+            $table->string('role_desc', 512)->default('')->comment('角色描述');
             $table->unsignedInteger('created_at')->default(0)->comment('创建时间');
             $table->unsignedInteger('updated_at')->default(0)->comment('更新时间');
+            //角色名称唯一索引
+            $table->unique('role_title', 'uk_manage_role_title');
         });
         //表注释
         \Illuminate\Support\Facades\DB::statement("ALTER TABLE `hb_manage_role` comment '管理员角色'");
