96e80b20a27f1b0c9ebb1e22689644df0054a336
joycezhang <787027175@qq.com>
2020-07-24 09:40:51 +0800

    增加字段

--- database/migrations/2020_07_08_092138_create_uc_member_table.php
+++ database/migrations/2020_07_08_092138_create_uc_member_table.php
@@ -26,6 +26,7 @@ class CreateUcMemberTable extends Migration
             $table->unsignedTinyInteger('user_state')->default(1)->comment('用户状态[1:正常,0:停用]');
             $table->bigInteger('reg_ip')->default(0)->comment('注册ip');
             $table->unsignedTinyInteger('is_super')->default(0)->comment('是否超级管理员[1 是 0 否]');
+            $table->unsignedInteger('group_id')->default(0)->comment('用户组');
             $table->enum('user_type', ['system', 'special', 'member'])->index('idx_member_user_type')->default('member')->comment('用户类型');
             $table->unsignedInteger('reg_date')->default(0)->comment('注册时间');
             $table->bigInteger('last_login_ip')->default(0)->comment('上次登录的IP(程序转换成数值类型)');
