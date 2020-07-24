fde7654877dbbb9e590e85d1a255212b8451f616
joycezhang <787027175@qq.com>
2020-07-24 09:40:06 +0800

    用户组数据填充

---
+++ database/seeds/MemberTableSeeder.php
@@ -0,0 +1,60 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class MemberTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+
+        //添加超级管理员角色
+        $group_id = \Illuminate\Support\Facades\DB::table('uc_member_group')->insertGetId([
+            'group_title' => '超级管理员',
+            'group_type' => 'system',
+            'is_system' => 1,
+            'is_default' => 1,
+            'group_remark' => '超级管理员角色，不可操作',
+            'created_at' => time(),
+            'updated_at' => time()
+        ]);
+
+        //添加超级管理员角色
+        \Illuminate\Support\Facades\DB::table('uc_member_group')->insertGetId([
+            'group_title' => '普通会员',
+            'group_type' => 'member',
+            'is_system' => 0,
+            'is_default' => 1,
+            'group_remark' => '默认普通会员组，不可操作',
+            'created_at' => time(),
+            'updated_at' => time()
+        ]);
+
+
+        //添加超级管理员
+        $manageId = \Illuminate\Support\Facades\DB::table('uc_member')->insertGetId([
+            'username' => 'admin',
+            'nickname' => '超级管理员',
+            'realname' => '超级管理员',
+            'password' => bcrypt('123456'),
+            'user_mobile' => '',
+            'user_salt' => '',
+            'user_email' => '',
+            'user_avatar' => '',
+            'user_state' => 1,
+            'user_type' => 'system',
+            'is_super' => 1,
+            'reg_date' => time(),
+            'group_id' => $group_id,
+            'reg_ip' => \JoyceZ\LaravelLib\Helpers\StrHelper::ip2long(),
+            'last_login_ip' => \JoyceZ\LaravelLib\Helpers\StrHelper::ip2long(),
+            'last_login_time' => time(),
+            'updated_at' => time()
+        ]);
+    }
+}
