426ce37f151a2f3015b534e8861f9e81bdf56245
joycezhang <787027175@qq.com>
2020-08-06 15:14:31 +0800

    增加索引

--- database/migrations/2020_07_08_092344_create_uc_member_identity_table.php
+++ database/migrations/2020_07_08_092344_create_uc_member_identity_table.php
@@ -15,10 +15,10 @@ class CreateUcMemberIdentityTable extends Migration
     {
         Schema::create('uc_member_identity', function (Blueprint $table) {
             $table->bigIncrements('identity_id');
-            $table->unsignedInteger('app_id')->default(0)->comment('内部应用appid');
-            $table->string('identity_from', 60)->default('')->comment('身份来源。按系统自定义可选值');
-            $table->unsignedBigInteger('uid')->default(0)->comment('用户id（每个身份来源只对应一个用户id）');
-            $table->unsignedBigInteger('outer_iid')->default(0)->comment('其他id（根据身份来源定位该id）');
+            $table->unsignedInteger('app_id')->default(0)->index('idx_sys_app_id')->comment('内部应用appid');
+            $table->string('identity_from', 60)->default('')->index('idx_member_identity_from')->comment('身份来源。按系统自定义可选值');
+            $table->unsignedBigInteger('uid')->default(0)->index('idx_member_uid')->comment('用户id（每个身份来源只对应一个用户id）');
+            $table->unsignedBigInteger('outer_iid')->default(0)->index('idx_member_identity_outer_iid')->comment('其他id（根据身份来源定位该id）');
             $table->string('identity_remark',512)->default('')->comment('备注');
             $table->unsignedInteger('created_at')->default(0)->comment('创建时间');
             $table->unsignedInteger('updated_at')->default(0)->comment('更新时间');
