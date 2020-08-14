df2da48d94b8f8bccf5260d969414849daf58d66
joycezhang <787027175@qq.com>
2020-08-14 10:54:16 +0800

    函数节流和防抖

--- public/static/ac/hbie.js
+++ public/static/ac/hbie.js
@@ -38,8 +38,13 @@
             },
             write: /\{(\d{1,2})(?:\:([xodQqb]))?\}/g,
             isURL: /^(?:ht|f)tp(?:s)?\:\/\/(?:[\w\-\.]+)\.\w+/i,
-            isUserName:/^[a-zA-Z0-9_]{5,16}$/,
-            isRealName:/^[a-zA-Z\u4e00-\u9fa5]+$/,
+            isUserName: /^[a-zA-Z0-9_]{5,16}$/,
+            isRealName: /^[a-zA-Z\u4e00-\u9fa5]+$/,
+            isCreditCode: /^[0-9A-HJ-NPQRTUWXY]{2}\d{6}[0-9A-HJ-NPQRTUWXY]{10}$/g,
+            isChineseName: /^[\u4e00-\u9fa5]+$/gi,
+            isIDCardNew: /^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}([0-9]|X)$/,
+            email: /^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]+$/,
+            mobile: /^1[23456789]\d{9}$/,
             cut: /[\x00-\xFF]/,
             getRealLen: {
                 r0: /[^\x00-\xFF]/g,
@@ -62,12 +67,34 @@
         isURL: function (str) {
             return HBIE.string.RegExps.isURL.test(str)
         },
+        email: function (value) {
+            return HBIE.string.RegExps.email.test(value);
+        },
+        mobile: function (value) {
+            return HBIE.string.RegExps.mobile.test(value)
+        },
+        //验证统一社会信用代码
+        isCreditCode: function (str) {
+            return HBIE.string.RegExps.isCreditCode.test(str)
+        },
+        /**
+         * 验证中文姓名
+         * @param str
+         * @returns {boolean}
+         */
+        isChineseName: function (str) {
+            return HBIE.string.RegExps.isChineseName.test(str)
+        },
+        //验证二代身份号
+        isIDCardNew: function (str) {
+            return HBIE.string.RegExps.isIDCardNew.test(str);
+        },
         /**
          * 用户名正则，5到16位（字母，数字，下划线）
          * @param str
          * @returns {boolean}
          */
-        isUserName: function(str){
+        isUserName: function (str) {
             return HBIE.string.RegExps.isUserName.test(str)
         },
         /**
@@ -91,10 +118,8 @@
             return Number(Math.random().toString().substr(3, len) + Date.now()).toString(36);
         }
     };
-    HBIE.validate={
-        rules:{
-
-        }
+    HBIE.validate = {
+        rules: {}
     }
     HBIE.Table = function (options) {
         if (!options) return;
@@ -143,6 +168,7 @@
         options.skin = options.skin || 'line';
         options.size = options.size || 'lg';
         options.async = (options.async !== undefined) ? options.async : true;
+        options.loading = options.loading || true;
         options.done = function (res, curr, count) {
             HBIE.image.loadImgMagnify();
             if (options.callback) options.callback(res, curr, count);
@@ -169,9 +195,27 @@
             }
         }, 50);
     };
+    /**
+     * 刷新数据
+     * @param options 参数，参考layui数据表格参数
+     */
+    HBIE.Table.prototype.reload = function (options) {
+        options = options || {
+            page: {
+                curr: 1
+            }
+        };
+        var _self = this;
+        var interval = setInterval(function () {
+            if (_self._table) {
+                _self._table.reload(_self.elem.replace(/#/g, ""), options);
+                clearInterval(interval);
+            }
+        }, 50);
+    };
     HBIE.image = {
-        IMG_COUNT : 0,
-        IMG_MAX_RECURSIVE_COUNT:6,
+        IMG_COUNT: 0,
+        IMG_MAX_RECURSIVE_COUNT: 6,
         loadImgMagnify: function () {
             setTimeout(function () {
                 try {
@@ -190,7 +234,7 @@
                 }
             }, 200);
         },
-        getImgId:function (obj) {
+        getImgId: function (obj) {
             HBIE.image.IMG_COUNT++;
             var id = obj.attr("id");
             if (id === undefined && HBIE.image.IMG_COUNT < HBIE.image.IMG_MAX_RECURSIVE_COUNT) {
@@ -203,10 +247,10 @@
             return id;
         }
     };
-    HBIE.date={
-        DEFAULT_TIME_FORMAT:'YYYY-MM-DD h:m:s',
-        formatParseTime:function (timestamp, format = '') {
-            format = format ==='' ? HBIE.date.DEFAULT_TIME_FORMAT : format;
+    HBIE.date = {
+        DEFAULT_TIME_FORMAT: 'YYYY-MM-DD h:m:s',
+        formatParseTime: function (timestamp, format = '') {
+            format = format === '' ? HBIE.date.DEFAULT_TIME_FORMAT : format;
             if (timestamp > 0) {
                 var date = new Date();
                 date.setTime(timestamp * 1000);
@@ -231,6 +275,70 @@
                 return time;
             } else {
                 return "";
+            }
+        }
+    };
+    let timer, flag;
+    HBIE.func = {
+        /**
+         * 函数节流
+         * 节流原理：在一定时间内，只能触发一次
+         * @param {Function} func 要执行的回调函数
+         * @param {Number} wait 延时的时间
+         * @param {Boolean} immediate 是否立即执行
+         * @returns {function(...[*]=)}
+         */
+        throttle: function (func, wait = 500, immediate = true) {
+            let timer, flag;
+            return function () {
+                if (immediate) {
+                    if (!flag) {
+                        flag = true;
+                        // 如果是立即执行，则在wait毫秒内开始时执行
+                        typeof func === 'function' && func();
+                        timer = setTimeout(() => {
+                            flag = false;
+                        }, wait);
+                    }
+                } else {
+                    if (!flag) {
+                        flag = true
+                        // 如果是非立即执行，则在wait毫秒内的结束处执行
+                        timer = setTimeout(() => {
+                            flag = false
+                            typeof func === 'function' && func();
+                        }, wait);
+                    }
+
+                }
+            }
+        },
+        /**
+         * 函数防抖
+         * 防抖原理：一定时间内，只有最后一次操作，再过wait毫秒后才执行函数
+         * @param {Function} func 要执行的回调函数
+         * @param {Number} wait 延时的时间
+         * @param {Boolean} immediate 是否立即执行
+         * @returns {function(...[*]=)}
+         */
+        debounce: function (func, wait = 500, immediate = false) {
+            let timeout;
+            return function () {
+                // 清除定时器
+                if (timeout !== null) clearTimeout(timeout);
+                // 立即执行，此类情况一般用不到
+                if (immediate) {
+                    var callNow = !timeout;
+                    timeout = setTimeout(function () {
+                        timeout = null;
+                    }, wait);
+                    if (callNow) typeof func === 'function' && func();
+                } else {
+                    // 设置定时器，当最后一次操作后，timeout不会再被清除，所以在延时wait毫秒后执行func回调方法
+                    timeout = setTimeout(function () {
+                        typeof func === 'function' && func();
+                    }, wait);
+                }
             }
         }
     }
