98b6638d49654abe118c4fcf3ae56eedb5760ed5
joycezhang <787027175@qq.com>
2020-11-18 16:24:59 +0800

    去掉多余的use

--- app/Http/Controllers/Manage/Index.php
+++ app/Http/Controllers/Manage/Index.php
@@ -3,13 +3,9 @@ declare (strict_types=1);
 
 namespace App\Http\Controllers\Manage;
 
-
-use Illuminate\Http\Request;
-
 class Index extends ManageController
 {
-    public function index(Request $request){
-
+    public function index(){
        return $this->view();
     }
 
