98ca0bb7921b6f87d3d4fe06468f222f4a723467
joycezhang <787027175@qq.com>
2020-11-18 17:05:41 +0800

    解析数据

--- app/Services/Repositories/CMS/ChannelRepo.php
+++ app/Services/Repositories/CMS/ChannelRepo.php
@@ -6,6 +6,8 @@ namespace App\Services\Repositories\CMS;
 
 use App\Services\Models\CMS\ChannelModel;
 use App\Services\Repositories\CMS\Interfaces\IChannel;
+use Illuminate\Support\Facades\Storage;
+use JoyceZ\LaravelLib\Helpers\DateHelper;
 use JoyceZ\LaravelLib\Repositories\BaseRepository;
 
 /**
@@ -20,5 +22,17 @@ class ChannelRepo extends BaseRepository implements IChannel
     public function __construct(ChannelModel $model)
     {
         parent::__construct($model);
+    }
+
+    public function parseDataRow(array $row): array
+    {
+        if (isset($row['channel_icon'])) {
+            $row['channel_icon_url'] = !empty($row['channel_icon']) ? asset(Storage::url($row['channel_icon'])) : '';
+        }
+        if (isset($row['created_at'])) {
+            $row['created_at_txt'] = DateHelper::formatParseTime((int)$row['created_at']);
+            $row['created_at_ago'] = DateHelper::formatDateLongAgo((int)$row['created_at']);
+        }
+        return $row;
     }
 }
