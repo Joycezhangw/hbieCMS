5735a6f207abf21e021cd3a6a91b23be2fcf37b3
joycezhang <787027175@qq.com>
2021-05-15 11:28:29 +0800

    调整 服务发现 结构

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -3,35 +3,6 @@
 
 namespace App\Providers;
 
-
-use App\Services\Repositories\CMS\ArticleRepo;
-use App\Services\Repositories\CMS\ChannelRepo;
-use App\Services\Repositories\CMS\Interfaces\IArticle;
-use App\Services\Repositories\CMS\Interfaces\IChannel;
-use App\Services\Repositories\Manage\Interfaces\IManage;
-use App\Services\Repositories\Manage\Interfaces\IManageLog;
-use App\Services\Repositories\Manage\Interfaces\IManageModule;
-use App\Services\Repositories\Manage\Interfaces\IManageRole;
-use App\Services\Repositories\Manage\ManageLogRepo;
-use App\Services\Repositories\Manage\ManageModuleRepo;
-use App\Services\Repositories\Manage\ManageRepo;
-use App\Services\Repositories\Manage\ManageRoleRepo;
-use App\Services\Repositories\System\AlbumFileRepo;
-use App\Services\Repositories\System\AlbumRepo;
-use App\Services\Repositories\System\DistrictRepo;
-use App\Services\Repositories\System\Interfaces\IAlbum;
-use App\Services\Repositories\System\Interfaces\IAlbumFile;
-use App\Services\Repositories\System\Interfaces\IDistrict;
-use App\Services\Repositories\System\Interfaces\ISlide;
-use App\Services\Repositories\System\Interfaces\ISysMpPages;
-use App\Services\Repositories\System\Interfaces\IWebLogError;
-use App\Services\Repositories\System\SlideRepo;
-use App\Services\Repositories\System\SysMpPagesRepo;
-use App\Services\Repositories\System\WebLogErrorRepo;
-use App\Services\Repositories\UCenter\Interfaces\IMember;
-use App\Services\Repositories\UCenter\Interfaces\IMemberGroup;
-use App\Services\Repositories\UCenter\MemberGroupRepo;
-use App\Services\Repositories\UCenter\MemberRepo;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
 class RepositoryServiceProvider extends ServiceProvider
@@ -45,19 +16,21 @@ class RepositoryServiceProvider extends ServiceProvider
 
     public function register()
     {
-        $this->app->bind(IManage::class, ManageRepo::class);//管理员
-        $this->app->bind(IManageModule::class, ManageModuleRepo::class);//管理员权限模块
-        $this->app->bind(IChannel::class, ChannelRepo::class);//内容栏目
-        $this->app->bind(IArticle::class, ArticleRepo::class);//内容
-        $this->app->bind(IAlbumFile::class, AlbumFileRepo::class);//附件
-        $this->app->bind(IAlbum::class, AlbumRepo::class);//附件专辑
-        $this->app->bind(ISlide::class, SlideRepo::class);//幻灯片
-        $this->app->bind(IManageLog::class, ManageLogRepo::class);//管理员日志
-        $this->app->bind(IManageRole::class, ManageRoleRepo::class);//管理员角色
-        $this->app->bind(IMember::class, MemberRepo::class);//用户管理
-        $this->app->bind(IMemberGroup::class, MemberGroupRepo::class);//用户组管理
-        $this->app->bind(IWebLogError::class, WebLogErrorRepo::class);//前端错误日志
-        $this->app->bind(IDistrict::class, DistrictRepo::class);//行政区域划分
-        $this->app->bind(ISysMpPages::class,SysMpPagesRepo::class);//金刚
+        //平台管理员
+        if (class_exists(\App\Services\Repositories\Manage\Providers\RepositoryServiceProvider::class)) {
+            $this->app->register(\App\Services\Repositories\Manage\Providers\RepositoryServiceProvider::class);
+        }
+        //系统
+        if (class_exists(\App\Services\Repositories\System\Providers\RepositoryServiceProvider::class)) {
+            $this->app->register(\App\Services\Repositories\System\Providers\RepositoryServiceProvider::class);
+        }
+        //会员
+        if (class_exists(\App\Services\Repositories\UCenter\Providers\RepositoryServiceProvider::class)) {
+            $this->app->register(\App\Services\Repositories\UCenter\Providers\RepositoryServiceProvider::class);
+        }
+        //内容
+        if (class_exists(\App\Services\Repositories\CMS\Providers\RepositoryServiceProvider::class)) {
+            $this->app->register(\App\Services\Repositories\CMS\Providers\RepositoryServiceProvider::class);
+        }
     }
 }
